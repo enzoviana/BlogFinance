73b28a2490dee58eaa20027dded57dc07042b603
enzoviana <69684975+enzoviana@users.noreply.github.com>
2022-02-27 22:32:37 +0000

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -13,10 +13,11 @@ class MyDocument extends Document {
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1139991563947565"
          crossorigin="anonymous"></script>
           <meta charset="UTF-8"></meta>
-          <meta name="description" content="Suivez le blog pour obtenir des conseils financiers personnels pour les femmes et les hommes sur l'épargne et l'investissement, des astuces de budgétisation et des articles inspirants sur les personnes qui atteignent la liberté financière. " />
+          <meta name="description" content="Pour obtenir des conseils financiers personnels sur l'épargne et l'investissement, des astuces et des articles inspirants sur la liberté financière." />
           <meta http-equiv = "content-language" content = "fr"></meta>
           <link rel="canonical" href="https://www.financefacile.fr" />
-          <title>FinanceFacile</title>
+          <link rel="apple-touch-icon" href="../public/apple-touch-icon.jpg"></link>
+          <title>Liberté financière - FinanceFacile</title>
         </Head>
         <body>
           <Main />
