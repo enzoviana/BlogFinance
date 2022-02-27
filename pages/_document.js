e7b3a4d0c79467c1c5f34fe7d07133553a2c450e
enzoviana <69684975+enzoviana@users.noreply.github.com>
2022-02-27 23:15:43 +0000

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -17,12 +17,18 @@ class MyDocument extends Document {
           <meta http-equiv = "content-language" content = "fr"></meta>
           <link rel="canonical" href="https://www.financefacile.fr" />
           <link rel="apple-touch-icon" href="../public/apple-touch-icon.jpg"></link>
+          <link rel="icon" type="image/x-icon" href="../public/favicon.ico"></link>
           <title>Liberté financière - FinanceFacile</title>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
+        <footer>
+        <div id="fb-root"></div>
+              <script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v13.0" nonce="6gIhfXSP"></script>
+              <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Partager</a></div>
+        </footer>
       </Html>
     );
   }
